9a3b72f20f339f685f70f446183c0a84ded91793
Nguyễn Công Vũ <nguyencongvu2801@gmail.com>
2023-07-17 18:46:18 +0700

    Update code

--- Login/js/vendor/login.js
+++ Login/js/vendor/login.js
@@ -4,7 +4,32 @@ const formOpenBtn = document.querySelector("#form-open"),
   formCloseBtn = document.querySelector(".form_close"),
   signupBtn = document.querySelector("#signup"),
   loginBtn = document.querySelector("#login"),
-  pwShowHide = document.querySelectorAll(".pw_hide");
+  pwShowHide = document.querySelectorAll(".pw_hide"),
+  loginForm = document.querySelector(".login_form"),
+  signupForm = document.querySelector(".signup_form"),
+  logoutBtn = document.querySelector("#logout"),
+  userInfo = document.querySelector("#user-info");
+
+// Simulated login functionality (replace with your actual authentication logic)
+function login(email, password) {
+  // Simulating successful login
+  if (email === "admin@gmail.com" && password === "admin123") {
+    return {
+      name: "Admin",
+    };
+  } else {
+    return null;
+  }
+}
+
+// Function to update UI after successful login
+function updateUIAfterLogin(user) {
+  userInfo.textContent = user.name;
+  userInfo.style.display = "inline-block";
+  logoutBtn.style.display = "inline-block";
+  loginForm.style.display = "none";
+  signupForm.style.display = "none";
+}
 
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
@@ -30,3 +55,72 @@ loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.remove("active");
 });
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const emailInput = loginForm.querySelector("input[type='email']");
+  const passwordInput = loginForm.querySelector("input[type='password']");
+  const user = login(emailInput.value, passwordInput.value);
+  if (user) {
+    updateUIAfterLogin(user);
+  } else {
+    alert("Invalid credentials. Please try again.");
+  }
+});
+
+logoutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  userInfo.style.display = "none";
+  logoutBtn.style.display = "none";
+  loginForm.style.display = "block";
+  signupForm.style.display = "none";
+});
+
+// Function to reset UI state after logging out
+function resetUIState() {
+  userInfo.style.display = "none";
+  logoutBtn.style.display = "none";
+  loginBtn.style.display = "inline-block";
+  loginForm.style.display = "block";
+  signupForm.style.display = "none";
+  formOpenBtn.style.display = "inline-block";
+
+  const toggleFormDisplay = (formToShow, formToHide) => {
+    formContainer.classList.add("active");
+    formToShow.style.display = "block";
+    formToHide.style.display = "none";
+  };
+
+  signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleFormDisplay(signupForm, loginForm);
+  });
+
+  loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleFormDisplay(loginForm, signupForm);
+  });
+}
+
+// Function to update UI after successful login
+function updateUIAfterLogin(user) {
+  userInfo.textContent = user.name;
+  userInfo.style.display = "inline-block";
+  userInfo.style.color = "ghostwhite";
+  userInfo.style.margin = "16px";
+  logoutBtn.style.display = "inline-block";
+  loginBtn.style.display = "none";
+  loginForm.style.display = "none";
+  signupForm.style.display = "none";
+  home.classList.remove("show");
+  formOpenBtn.style.display = "none";
+}
+
+logoutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  resetUIState();
+});
+
+
+
+
